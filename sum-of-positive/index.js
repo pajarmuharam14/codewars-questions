d90b3929ff3c7a54defbe4231c85886414c4550d
pajarmuharam14 <pajarmuharam321@gmail.com>
2024-02-05 14:36:53 +0700

    add sum of positive challenge

---
+++ sum-of-positive/index.js
@@ -0,0 +1,13 @@
+function positiveSum(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      result;
+    } else {
+      result += arr[i];
+    }
+  }
+  return result;
+}
+
+console.log(positiveSum([1, 2, 3, 4, 5]));

019b1f5dc55b8be37eb93e055d74cdd475c0edd4
pajarmuharam14 <pajarmuharam321@gmail.com>
2024-02-06 09:37:05 +0700

    add description

--- sum-of-positive/index.js
+++ sum-of-positive/index.js
@@ -1,3 +1,7 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// example [1,-4,7,12] => 1 + 7 + 12 = 20
+
 function positiveSum(arr) {
   let result = 0;
   for (let i = 0; i < arr.length; i++) {

5517e183b1643e6c5e2774acf121f86325a595d4
pajarmuharam14 <pajarmuharam321@gmail.com>
2024-02-10 09:51:31 +0700

    add summation challenge

---
+++ summation/index.js
@@ -0,0 +1,16 @@
+/*/Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+
+example
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+ /*/
+
+var summation = function (num) {
+  let number = 0;
+  for (let i = 1; i <= num; i++) {
+    number += i;
+  }
+  return number;
+};
+
+console.log(summation(8));
